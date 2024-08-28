49422f326991fafdf0921084857f38514fe068cf
nyoungno Ko <yghjbm@gmail.com>
2024-08-28 12:03:35 +0900

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import logo from "./img/ABC.png";
+import {
+  Button,
+  Container,
+  Nav,
+  Navbar,
+  Form,
+  NavDropdown,
+  Row,
+  Col,
+  Card,
+} from "react-bootstrap";
+import "./App.css";
+import data from "./Data.js";
+import { Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  let [shoes] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/detail" element={<div>상세페이지</div>} />
+      </Routes>
+
+      <Navbar bg="light" data-bs-theme="light" style={{ height: "97px" }}>
+        <Container>
+          <Navbar.Brand
+            href="#home"
+            style={{
+              width: "300px",
+              display: "flex",
+              justifyContent: "flex-start",
+            }}
+          >
+            <img
+              src={logo} // 로고 이미지 파일 경로
+              alt="ABC MART Logo"
+              style={{ width: "100px", height: "auto" }} // 로고 크기 조절
+            />
+          </Navbar.Brand>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="역대급 할인!"
+              className="me-2"
+              aria-label="Search"
+              style={{ minWidth: "400px" }}
+            />
+            <Button variant="outline-danger">Search</Button>
+          </Form>
+          <div
+            style={{
+              width: "300px",
+              display: "flex",
+              justifyContent: "flex-end",
+              fontWeight: "bolder",
+
+              gap: "20px",
+            }}
+          >
+            <Nav.Link href="#pricing">LOGIN</Nav.Link>
+            <Nav.Link href="#pricing">JOIN</Nav.Link>
+            <Nav.Link href="#pricing">CART</Nav.Link>
+            <Nav.Link href="#pricing">SHOP</Nav.Link>
+          </div>
+        </Container>
+      </Navbar>
+      <Navbar
+        style={{
+          backgroundColor: "red",
+          color: "white",
+          fontWeight: "bolder",
+          textAlign: "center",
+          height: "48px",
+        }}
+      >
+        <Container>
+          <div style={{ display: "flex", gap: "50px" }}>
+            <NavDropdown title="BRAND" id="navbarScrollingDropdown">
+              <NavDropdown.Item style={{ hover: "red" }} href="#action3">
+                NIKE
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action4">ADIDAS</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="MEN" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">신발</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">의류</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="WOMEN" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">신발</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">의류</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title="KIDS" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">신발</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">의류</NavDropdown.Item>
+            </NavDropdown>
+          </div>
+          <div style={{ display: "flex", gap: "50px" }}>
+            <Nav.Link href="#pricing">기획전</Nav.Link>
+            <Nav.Link href="#pricing">이벤트/쿠폰</Nav.Link>
+            <Nav.Link href="#pricing">ONLY ABC</Nav.Link>
+            <Nav.Link href="#pricing">KIDS마트</Nav.Link>
+            <Nav.Link href="#pricing">0월 쇼핑 혜택</Nav.Link>
+          </div>
+        </Container>
+      </Navbar>
+      <div className="main-bg"></div>
+      <h2 style={{ fontWeight: "bolder", padding: "30px" }}>
+        당신을 위한 맞춤 신상품
+      </h2>
+      <Container>
+        <Row>
+          {/* <Col>
+            <Card1 shoes={shoes[0]}></Card1>
+          </Col>
+          <Col>
+            <Card1 shoes={shoes[1]}></Card1>
+          </Col>
+          <Col>
+            <Card1 shoes={shoes[2]}></Card1>
+          </Col>
+          <Col>
+            <Card1 shoes={shoes[3]}></Card1>
+          </Col>
+          <Col>
+            <Card1 shoes={shoes[4]}></Card1>
+          </Col> */}
+          {shoes.map((a, i) => {
+            return (
+              <Col>
+                <Card1 shoes={shoes[i]}></Card1>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+function Card1(props) {
+  return (
+    <Card style={{ width: "15rem" }}>
+      <Card.Img variant="top" src={props.shoes.image} />
+      <Card.Body>
+        <Card.Title style={{ fontWeight: "bolder" }}>
+          {props.shoes.title}
+        </Card.Title>
+        <Card.Text style={{ fontSize: "12px", height: "36px" }}>
+          {props.shoes.content}
+        </Card.Text>
+        <p style={{ color: "red", fontWeight: "bolder" }}>
+          {props.shoes.price}원
+        </p>
+        <Button variant="primary">주문하기</Button>
+      </Card.Body>
+    </Card>
+  );
+}
---
+++ src/Data.js
@@ -0,0 +1,48 @@
+let data = [
+  {
+    id: 0,
+    title: "아식스",
+    content: "GT-2160",
+    price: 139000,
+    image:
+      "https://image.a-rt.com/art/product/2024/08/37458_1722560940298.jpg?shrink=224:224",
+  },
+
+  {
+    id: 1,
+    title: "나이키",
+    content: "우먼스 나이키 코트 비전 알타 레더",
+    price: 99000,
+    image:
+      "https://image.a-rt.com/art/product/2022/01/60008_1642143249212.jpg?shrink=590:590",
+  },
+
+  {
+    id: 2,
+    title: "푸마",
+    content: "파크 라이프스타일 오리지날",
+    price: 89000,
+    image:
+      "https://image.a-rt.com/art/product/2024/01/44743_1706606864426.jpg?shrink=590:590",
+  },
+
+  {
+    id: 3,
+    title: "스테파노로시",
+    content: "밀라카",
+    price: 69000,
+    image:
+      "https://image.a-rt.com/art/product/2022/04/91504_1650446298804.jpg?shrink=590:590",
+  },
+
+  {
+    id: 4,
+    title: "나이키",
+    content: "리프트 (보이그레이드/프리스쿨 보이즈)밀라카",
+    price: 69000,
+    image:
+      "https://image.a-rt.com/art/product/2020/04/36602_1586148400245.jpg?shrink=590:590",
+  },
+];
+
+export default data;
